139183f12f83f160c934be0b06d662876a4b86ae
Eric Ferguson <ferguson.eric.r@gmail.com>
2021-02-15 11:40:34 -0800

    set up useState and useEffect/axios to call API

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ A cryptocurrency finance price tracker app.
 
 ## Outcome
 Be able to search currencies and have them display on the browser while the prices will update live. All of the data will pull from  a public API. 
+
+### Steps
+    - get API key, set parameters for key,
+    https://www.coingecko.com/en/api
+
+    - import Axios + useState,useEffect 
+
+    - set useState (coins, setCoins) to []
+
+    - set useEffect w/ axios to .get() API key
+        - .then set to setCoins
+        - tested, data passes 
+    
+    - 
--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,7 +1,23 @@
 
 import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [ coins, setCoins ] = useState([])
+useEffect(() => {
+  axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data)
+    })
+
+    .catch(error => console.log(error))
+}, [])
+
   return (
     <div className="App">
       <h1>Crypto-Tracker</h1>
